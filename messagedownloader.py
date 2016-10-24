c9f370006350effd8cdc14048581fbe196191eb4
yodalee <lc85301@gmail.com>
2016-10-24 11:38:19 +0800

    require user login

--- messagedownloader.py
+++ messagedownloader.py
@@ -2,20 +2,17 @@
 #/usr/bin/env python
 import sys
 
-import logging
-import json
-import random
-import urllib2
-
 import webapp2
+from google.appengine.api import users
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
         self.response.headers["Content-Type"] = "text/plain"
-        self.response.write("Hello, World!")
+        self.response.write("Hello, {0}\n".format(user.nickname()))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),

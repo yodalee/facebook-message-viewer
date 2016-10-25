ef0da8e427d471d426bef2da0de117f30de27f90
yodalee <lc85301@gmail.com>
2016-10-25 13:48:41 +0800

    first version that can upload data to blobstore

--- messagedownloader.py
+++ messagedownloader.py
@@ -4,16 +4,53 @@ import sys
 
 import webapp2
 from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext import ndb
+from google.appengine.ext.webapp import blobstore_handlers
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+# Basic datastruct in user blob
+class UserMessage(ndb.Model):
+    user = ndb.StringProperty()
+    blob_key = ndb.BlobKeyProperty()
+
+
+class MessageUploadFormHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        try:
+            upload = self.get_uploads()[0]
+            user_message = UserMessage(
+                user=users.get_current_user().user_id(),
+                blob_key=upload.key())
+            user_message.put()
+
+            self.redirect('/view')
+
+        except:
+            self.error(500)
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        self.response.headers["Content-Type"] = "text/plain"
-        self.response.write("Hello, {0}\n".format(user.nickname()))
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write("""
+<html><body>
+<form action="{0}" method="POST" enctype="multipart/form-data">
+    Upload Message.htm: <input type="file" name="file"><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
+</body></html>""".format(upload_url))
+
+
+class MessageViewHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Hello World")
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/upload', MessageUploadFormHandler),
+    ('/view', MessageViewHandler),
 ], debug=True)

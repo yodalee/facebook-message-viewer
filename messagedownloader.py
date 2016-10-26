3dd42552cea2467492ab1aa90f2b7e1c12332b2e
yodalee <lc85301@gmail.com>
2016-10-26 10:34:06 +0800

    check database status in template

--- messagedownloader.py
+++ messagedownloader.py
@@ -23,15 +23,19 @@ sys.setdefaultencoding("utf-8")
 class UserMessage(ndb.Model):
     user = ndb.StringProperty()
     blob_key = ndb.BlobKeyProperty()
+    isReady = ndb.BooleanProperty()
 
 
 class MessageUploadFormHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         try:
             upload = self.get_uploads()[0]
+            blob_key = upload.key()
+            user_id = users.get_current_user().user_id()
             user_message = UserMessage(
-                user=users.get_current_user().user_id(),
-                blob_key=upload.key())
+                user=user_id,
+                isReady=False,
+                blob_key=blob_key)
             user_message.put()
 
             self.redirect('/view')
@@ -56,12 +60,18 @@ class MessageViewHandler(webapp2.RequestHandler):
     def get(self):
         query = UserMessage.query(
                 UserMessage.user == users.get_current_user().user_id())
-        msgblob = query.fetch()
-        if len(msgblob) == 0:
-            self.redirect('upload')
+        userdata = query.fetch()
+        template_values = {
+            'userExist': True,
+            'hasData': True,
+        }
+        if len(userdata) == 0:
+            template_values['userExist'] = False
+        else:
+            template_values['hasData'] = userdata[0].isReady
 
         template = JINJA_ENVIRONMENT.get_template('view.html')
-        self.response.write(template.render())
+        self.response.write(template.render(template_values))
 
 
 class RedirectHandler(webapp2.RequestHandler):
@@ -72,5 +82,6 @@ app = webapp2.WSGIApplication([
     ('/view', MessageViewHandler),
     ('/upload', MessageUploadForm),
     ('/uploadhandler', MessageUploadFormHandler),
+    ('/update_user', MessageProcessHandler),
     ('/.*', RedirectHandler),
 ], debug=True)

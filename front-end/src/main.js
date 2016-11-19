23f3c138aae0aa1560cb5abe8e9b8b9a3bb125bd
Inndy <inndy.tw@gmail.com>
2016-11-19 16:03:52 +0800

    Add vue-datepicker

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import VueDatepicker from 'vue-datepicker'
 import VueMoment from 'vue-moment'
 import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.use(VueMoment)
+Vue.component('date-picker', VueDatepicker)
 
 /* eslint-disable no-new */
 new Vue({
